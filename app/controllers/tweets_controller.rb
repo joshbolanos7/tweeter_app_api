435d99d24a0b61c03b8d4db826e7b971ea2faba8
Joshua Bolanos <josh.bolanos7@gmail.com>
2021-11-02 23:38:01 -0400

    commit 9: tweets update.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,6 +19,15 @@ class TweetsController < ApplicationController
             render(status:422, json: { tweet: tweet })
         end
     end
+    
+    def update
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render(json: { tweet: tweet})
+    end
+
+    
+
 private 
 
     def tweet_params
